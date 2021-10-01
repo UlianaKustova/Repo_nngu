d31b3f71ac657ee3ff1657d6ec41695882edc4fd
Uliana Kustova <ul.kustovak2@gmail.com>
2021-10-01 23:21:56 +0300

    update

--- lesson_1/class_work/index.js
+++ lesson_1/class_work/index.js
@@ -1,2 +1,20 @@
 console.log("Hello world!");
 
+let our_planet = "Earth"
+console.log(our_planet)
+
+let time = "21:58"
+console.log(time)
+
+let texts = "24"
+console.log(texts)
+
+let lastname = "Restova"
+console.log(lastname)
+
+let name = "Uliana"
+console.log(our_planet)
+
+let middlename = "Mihailovna"
+console.log(our_planet)
+
--- lesson_1/home_work/index.js
+++ lesson_1/home_work/index.js
@@ -1,2 +1,28 @@
-console.log("Hello world bbb");
+const age4 = 15;
+console.log(age4)
 
+let name2 = "Uliana";
+console.log(name2)
+
+let userName00 = 'Uliana';
+console.log(userName00)
+
+let isAdmin = false;
+console.log(isAdmin)
+
+let isOpen = true;
+console.log(isOpen)
+
+let username7 = null;
+console.log(username7)
+
+let Username;
+console.log(Username)
+
+let user = {
+    name: "Uliana",
+    age: 15
+};
+console.log(user)
+
+typeof (x)
