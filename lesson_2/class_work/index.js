6de632fbe113b64649dadd07e497c7f8807e9bd5
Uliana Kustova <ul.kustovak2@gmail.com>
2021-10-08 21:44:39 +0300

    update

---
+++ lesson_2/class_work/index.js
@@ -0,0 +1,81 @@
+/*                                                                        СТРОКОВОЕ STRING */
+
+/*const firstvalue = String(5);
+console.log(typeof firstvalue)
+
+const obj = {
+    foo: 19,
+}
+close.log(tupeof, String(obj.foo));*/
+
+/*  */
+
+/*                                                                         ЧИСЛЕННОЕ NUMBER */
+
+/*let testvalue = "123456zx"
+console.log(typeof testvalue);
+
+testvalue = Number(testvalue);
+console.log(typeof testvalue)
+
+const isusers = true;
+const isadmins = false;
+console.log(Number(isusers), Number(isadmins));
+console.log(Number(isusers));
+
+const voidString = ""
+console.log(Number(voidString));*/
+
+// console.log(16 % 2)
+// console.log(2 ** 10)
+
+/* поддерживает +, -, :, *,
+остаток от деления%, возведение в степень** */
+
+/*                                                                         ЛОГИЧЕСКОЕ BOOLEAN */
+
+// const value = "1";
+// console.log(Boolean(value));
+
+// const obj = [];
+// console.log(Boolean(obj));
+
+/* 1- андефаинд при чис. образ. становится NuN а не 0
+2- 0 и строки из пробелов это тру */
+
+/*                                                                         УНАРНЫЙ ПЛЮС */
+
+// const value = "5";
+// console.log(typeof value, typeof +value);
+
+
+/*                                                                          БИНАРНЫЙ ПЛЮС */
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 4 + null;
+// console.log(sum);
+
+// const Stringvalue = 'Uliana';
+// const Numbervalue = 15;
+// console.log(+Stringvalue + Numbervalue);
+// console.log(typeof (Numbervalue + Stringvalue));
+
+// const firstname = 'Uliana';
+// const lastname = 'Kustova';
+// console.log(firstname + " " + lastname);
+// console.log(` Hello i am ${firstname} ${lastname}`)
+
+
+
+
+
+
+
+
+
+
+
