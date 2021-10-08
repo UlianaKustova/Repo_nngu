57791df318ea6a7e4dded20534bff011ba9e48a9
Uliana Kustova <ul.kustovak2@gmail.com>
2021-10-08 22:14:21 +0300

    update

---
+++ lesson_2/home_work/index.js
@@ -0,0 +1,31 @@
+let a = "код";
+let b = "кот";
+console.log(b > a);
+
+console.log(null == 0)
+
+console.log(2 > "3")
+
+console.log(null - false + true)
+
+console.log(1 / "l")
+
+console.log("2" * "3")
+
+console.log(4 + 5 + "0")
+
+console.log("l" + 4 + 5)
+
+console.log("4" - 2)
+
+console.log("4" - "4x")
+
+console.log("23" == 23)
+
+console.log(null == false)
+
+console.log(" -4 " / 0 + 1)
+
+console.log(1 === "1")
+
+console.log("2" > 10)
