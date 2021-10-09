b1554e086b6cbcbb73d0c28dd6a18e87f6d0d4ae
Uliana Kustova <ul.kustovak2@gmail.com>
2021-10-09 11:20:14 +0300

    update

--- lesson_3/class_work/index.js
+++ lesson_3/class_work/index.js
@@ -1,6 +1,5 @@
 /* ИНСТРУКЦИЯ IF (УСЛОВИЕ){ИНСТРУКЦИЯ} */
 // if (1) {
-
 //     console.log(1)
 // }
 // if (2) {
@@ -16,18 +15,97 @@
 // }
 // console.log('FINISH')
 
-console.log('START')
-const value = 0
-if (value === 0) {
-    console.log(0, '= value')
-} else if ('value, ' > 1) {
-    console.log('value, ' > 1)
-} else if ('value, ' < -1) {
-    console.log('value, ' < -1)
+// console.log('START')
+// const value = 0
+// if (value === 0) {
+//     console.log(0, '= value')
+// } else if ('value, ' > 1) {
+//     console.log('value, ' > 1)
+// } else if ('value, ' < -1) {
+//     console.log('value, ' < -1)
+// } else {
+//     console.log(value)
+// }
+// console.log('FINISH') 
+
+/* ТЕРНАРНЫЙ ОПЕРАТОР */
+// const age = 20;
+// const results = age >= 18 ? "Можно" : "Нельзя";
+// console.log(results);
+
+/* const name = 'Uliana'
+switch ("Uliana") {
+    case "rrr":
+        console.log("RRR");
+        break
+    case "sss":
+        console.log("SSS");
+        break
+    default:
+        console.log("Нет в списке");
+} */
+
+/* const value = 15
+const value2 = 3
+const value3 = 55
+
+if (value > value2) {
+    value2 > value3 ? console.log(value) : console.log(value3)
 } else {
-    console.log(value)
+    value2 > value3 ? console.log(value2) : console.log(value3)
+} */
+/*
+const name = 'Uliana'
+switch ("Uliana") {
+    case "rrr":
+        console.log("RRR");
+        break
+    case "sss":
+        console.log("SSS");
+        break
+    default:
+        console.log("Нет в списке");
 }
-console.log('FINISH')
+
+let age = 15
+switch (15) {
+    case 18:
+        console.log(18)
+    case 33:
+        console.log(33)
+        break
+    default:
+        console.log("Ваш возраст?")
+} */
+
+/* const admin = true
+switch (admin) {
+    case true:
+        console.log("Yes")
+    case false:
+        console.log("No")
+    default:
+        console.log("Admin?")
+} */
+
+/* const user = 'users'
+switch (user) {
+    case "users":
+        console.log("user info")
+        break
+    case "admin":
+        console.log("admin info")
+        break
+    case "manager":
+        console.log("manader info")
+        break
+    default:
+        console.log("no info")
+}
+ */
+
+
+
 
 
 
